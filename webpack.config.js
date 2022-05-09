fe64a7973af1ca6dc49136540f5937a51100d6f9
Sylvain DEPARTE <sdeparte@localhost.localdomain>
2022-05-09 07:57:17 +0200

    Set default environment to 'Production'

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
-    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'prod');
 }
 
 Encore
