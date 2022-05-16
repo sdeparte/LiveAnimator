9299ddc40cd5e237d37d121ff0f396c94830e169
Sylvain DEPARTE <sdeparte@localhost.localdomain>
2022-05-16 07:57:38 +0200

    noSound property of music event is now a real boolean

--- assets/js/components/WhatTheSong.vue
+++ assets/js/components/WhatTheSong.vue
@@ -64,7 +64,7 @@ export default {
       if (data.type == 'music') {
         this.author = '';
         this.song = '';
-        this.noSound = data.noSound === 'true';
+        this.noSound = data.noSound;
 
         document.getElementById("myPlayer").classList.add('diskChanging');
 
